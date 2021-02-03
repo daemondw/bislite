50e5eac9b8f87ba401846080deb4d28e9322489e
Dmytro Antonenko <daemondw@gmail.com>
2021-02-03 23:16:09 +0100

    added files

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.slider__inner').slick({
+        arrows: false,
+        dots: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+});
+});
