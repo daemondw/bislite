9db1356e4b5d27c04929bb9843a00ba9a961f415
Dmytro Antonenko <daemondw@gmail.com>
2021-02-05 07:51:33 +0100

    section works

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
-$(document).ready(function(){
+$(function(){
+
     $('.slider__inner').slick({
         arrows: false,
         dots: true,
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1
 });
+
+    $('.works__inner').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/slider/slide-left.svg" alt=""></button>',
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/slider/slide-right.svg" alt=""></button>',
+    });
+
 });
